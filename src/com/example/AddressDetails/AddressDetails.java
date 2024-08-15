818ba3108880a73d5be745b75d27eff79b92f28c
Samrendra Pratap Singh <153061395+solo-source@users.noreply.github.com>
2024-08-16 00:59:26 +0530

    Started Learning the Infosys Course. Created a Sample Class for demonstrating the use of Variables and and println() method.

---
+++ src/com/example/AddressDetails/AddressDetails.java
@@ -0,0 +1,15 @@
+package com.example.AddressDetails;
+
+public class AddressDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String door_no = "D089";
+        String street_name = "St. Louis Street";
+        String city_name = "Springfield";
+        int zip_code = 62729;
+        
+        System.out.println("Door No: " + door_no + "\n" + "Street: " +  street_name + "\n" + "City: " + city_name + "\n" +  "Zip Code: " + zip_code);
+	}
+
+}
