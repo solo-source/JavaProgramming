cca831b1795d6e0ba51ac440611790ba29d556f3
Samrendra Pratap Singh <153061395+solo-source@users.noreply.github.com>
2024-08-17 02:06:32 +0530

    Studied operators and Data Types in java.

---
+++ src/com/example/Demo/VariableBehaviour.java
@@ -0,0 +1,18 @@
+package com.example.Demo;
+
+public class VariableBehaviour {
+	
+	int i;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j;
+		
+//		System.out.println(i); /*
+//								Find out about the error 
+//								Unresolved compilation problem: 
+//								Cannot make a static reference to the non-static field i
+//		 						*/
+//		System.out.println(j*10);
+	}
+
+}
