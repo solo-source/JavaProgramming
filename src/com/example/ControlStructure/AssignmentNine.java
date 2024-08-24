92578e789665c8435334b64feb272c392e475c91
Samrendra Pratap Singh <153061395+solo-source@users.noreply.github.com>
2024-08-24 23:02:02 +0530

    Solved the problem ion Assignment 9th and started assignment 10th. Need to improve my logic building and algorithm techniques. Need to practice more.

--- src/com/example/ControlStructure/AssignmentNine.java
+++ src/com/example/ControlStructure/AssignmentNine.java
@@ -14,40 +14,54 @@ public class AssignmentNine {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int input, fakeInput;
+		int input, fakeInput, mainValue;
 		int counter = 0;
 		boolean status = false;
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Enter the number: ");
 		input = sc.nextInt();
-		fakeInput = input;
+		fakeInput = mainValue = input;
+		
 		//counting no of digits in input
 		while(input > 0) {
 			input /= 10;
 			counter++;
 		}
+//		System.out.println(counter);
 		
 		var arr = new int[counter];
 		int i = 0;
 		
-		while(input > 0 && i < counter) {
-			arr[i] = input % 10;
-			input /= 10;
-			i++;
+		//feeding each digit as an element of an array of size of digit
+		while(fakeInput > 0) {
+			if(i < counter) {
+				arr[i] = fakeInput % 10;
+				fakeInput /= 10;
+				i++;
+			}
 		}
 		
-		for(int j = 1; j < arr.length - 1; j++) {
-			if(arr[j] == arr[arr.length - j]) {
+//		for(int j = 0; j < arr.length; j++) {
+//			System.out.println(arr[j]);
+//		}
+//		
+//		System.out.println(arr.length);
+		
+		
+		//Checking for palindrome by comparing j'th and arr.length - (j + 1)'th elements of the array.
+		for(int j = 0; j < arr.length; j++) {
+			
+			if(arr[j] == arr[arr.length - (j + 1)]) {
 				status = true;
 			}
 			else
 				status = false;
 		}
 		if(status == true)
-			System.out.println(fakeInput + " is a palindrome");
+			System.out.println(mainValue + " is a palindrome");
 		else
-			System.out.println(fakeInput + " is not qa palindrome");
+			System.out.println(mainValue + " is not a palindrome");
 		
 		sc.close();
 	}
---
+++ src/com/example/ControlStructure/AssignmentTenth.java
@@ -0,0 +1,17 @@
+//Problem Statement
+//
+//Implement a program to find the number of rabbits and chickens in a farm. Given the number of heads 
+//and legs of the chickens and rabbits in a farm, identify and display the number of chickens and rabbits
+//in the farm.
+//
+//If the given input cannot make a valid number of rabbits and chickens, then display an appropriate message.
+package com.example.ControlStructure;
+
+public class AssignmentTenth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
